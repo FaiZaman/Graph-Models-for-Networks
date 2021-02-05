0ee58795920456a19c554249cf2d6f561580a3ff
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-02-05 14:52:13 +0000

    Looping through and checked first index for swapping

--- routing_algorithms/star_graph.py
+++ routing_algorithms/star_graph.py
@@ -1,15 +1,32 @@
 
 def star5_routing(source, destination):
 
+    # initialise path and current node
     path = [source]
     current_node = source
+    node_length = len(current_node)
 
+    # keep routing until the destination is reached
     while current_node != destination:
 
-        if current_node[0] != destination[0]:
-            break
+        # check what digit must be swapped
+        for index in range(0, node_length):
 
-        # path.append(next_node)
+            if index == 0:
+
+                if current_node[index] != destination[index]:
+
+                    # get digit and find its index in destination
+                    digit = current_node[index]
+                    destination_digit_index = destination.index(digit)
+
+                    current_node[index], current_node[destination_digit_index] =\
+                        current_node[destination_digit_index], current_node[index]
+                    
+                    break
+
+        print(current_node)
+        path.append(current_node)
 
     return path
 
