644f959516634ae640b666438a22b1d5659d69d8
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-02-05 15:38:54 +0000

    Renamed and finished routing for S5

--- routing_algorithms/Int_Nets_routing_psgg66.py
+++ routing_algorithms/Int_Nets_routing_psgg66.py
@@ -1,9 +1,9 @@
 
 def star5_routing(source, destination):
 
-    # initialise path and current node
-    path = [source]
-    current_node = source
+    # initialise route and current node
+    route = [source]
+    current_node = source.copy()
     node_length = len(current_node)
 
     # keep routing until the destination is reached
@@ -14,20 +14,30 @@ def star5_routing(source, destination):
 
             if index == 0:
 
+                # not equal means they should be swapped
                 if current_node[index] != destination[index]:
 
                     # get digit and find its index in destination
                     digit = current_node[index]
                     destination_digit_index = destination.index(digit)
 
+                    # swap
                     current_node[index], current_node[destination_digit_index] =\
                         current_node[destination_digit_index], current_node[index]
-                    
+
                     break
 
-        print(current_node)
-        path.append(current_node)
+            # not equal means they should be swapped
+            if current_node[index] != destination[index]:
 
-    return path
+                current_node[0], current_node[index] = current_node[index], current_node[0]
+                break
 
-star5_routing([1, 2, 3, 4], [4, 3, 2, 1])
+        route.append(current_node.copy())
+
+    return route
+
+
+route = star5_routing([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
+for node in route:
+    print(node)
