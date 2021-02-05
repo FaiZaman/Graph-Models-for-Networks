0def0470b777a07a51ced5bcc46b2951dc69ee8e
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-02-05 20:38:29 +0000

    Implemented all-to-all traffic pattern and recorded channel loads for S5

--- routing_algorithms/Int_Nets_routing_psgg66.py
+++ routing_algorithms/Int_Nets_routing_psgg66.py
@@ -1,4 +1,3 @@
-import numpy as np
 from itertools import permutations
 
 # takes source and destination nodes and returns list of nodes
@@ -48,16 +47,45 @@ def simulate_traffic():
     # generate the nodes of S5 and initialise channel loads matrix
     nodes = list(permutations([i for i in range(1, 6)]))
     nodes = [list(node) for node in nodes]
-    channel_loads_matrix = np.zeros((120, 120))
+    channel_loads_matrix = [[0 for x in range(120)] for y in range(120)] 
 
-    
+    # all-to-all traffic pattern
+    for source in nodes:
+        for destination in nodes:
 
+            # retrieve indices
+            source_index = nodes.index(source)
+            destination_index = nodes.index(destination)
+
+            # same source and destination means channel load is zero
+            if source == destination:
+
+                channel_loads_matrix[source_index][destination_index] = 0
+
+            else:
+
+                # compute the route b/w source and destination + get pairs of nodes from route
+                route = star5_routing(source, destination)
+                channels = list(zip(route, route[1:]))
+
+                # update channel loads
+                for channel in channels:
+
+                    # retrieve nodes
+                    node1, node2 = channel[0], channel[1]
+                    node1_index, node2_index = nodes.index(node1), nodes.index(node2)
+
+                    # update values
+                    channel_loads_matrix[node1_index][node2_index] += 1
+
+    for row in channel_loads_matrix:
+        print(row)
 
 
 simulate_traffic()
 
-"""
+
 route = star5_routing([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
 for node in route:
     print(node)
-"""
+
