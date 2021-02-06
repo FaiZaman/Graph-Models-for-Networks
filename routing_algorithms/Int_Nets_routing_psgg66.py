f6f410dd0252fcfe1ae675689a1133d6f3e128c5
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-02-06 16:31:22 +0000

    Finished implementing k-ary n-cube routing

--- routing_algorithms/Int_Nets_routing_psgg66.py
+++ routing_algorithms/Int_Nets_routing_psgg66.py
@@ -47,17 +47,13 @@ def star5_routing(source, destination):
 def simulate_traffic():
 
     # generate the nodes of S5 and initialise channel loads matrix
-    nodes = list(permutations([i for i in range(1, 4)]))
+    nodes = list(permutations([i for i in range(1, 5)]))
     nodes = [list(node) for node in nodes]
-    channel_loads_matrix = [[0 for x in range(6)] for y in range(6)] 
+    channel_loads_matrix = [[0 for x in range(24)] for y in range(24)] 
 
     # all-to-all traffic pattern
     for source in nodes:
         for destination in nodes:
-
-            # retrieve indices
-            source_index = nodes.index(source)
-            destination_index = nodes.index(destination)
 
             # same source and destination means channel load is zero; remain unchanged
             if source != destination:
@@ -76,7 +72,6 @@ def simulate_traffic():
                     # update values
                     channel_loads_matrix[node1_index][node2_index] += 1
 
-
     # printing the loads
     for i in range(0, len(channel_loads_matrix)):
         for channel in range(0, len(channel_loads_matrix)):
@@ -87,7 +82,9 @@ def simulate_traffic():
     return channel_loads_matrix
 
 
-channel_loads_matrix = simulate_traffic()
+#channel_loads_matrix = simulate_traffic()
+#for row in channel_loads_matrix:
+#    print(row)
 
 #route = star5_routing([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
 #for node in route:
@@ -134,10 +131,68 @@ def karyncube_routing(n, k, source, target):
     dimension = n - 1
 
     # keep routing until the target node is reached
-    while source != target:
+    while current_node != target:
+
+        u = current_node[dimension]
+        v = target[dimension]
 
         # if bits are equal move to next dimension
-        if current_node[dimension] != target[dimension]:
+        while current_node[dimension] != target[dimension]:
 
-            pass
+            # calculate both distances - with and without wrapping
+            non_wrap_distance = abs(u - v)
+            wrap_distance = k - non_wrap_distance
 
+            # break ties by increasing component
+            if non_wrap_distance == wrap_distance:
+
+                # wrap around switch
+                if current_node[dimension] == k - 1:
+                    current_node[dimension] = 0
+                else:
+                    current_node[dimension] += 1
+
+            # without wrapping around
+            elif non_wrap_distance < wrap_distance:
+
+                # determine whether increasing/decreasing component is required and update it
+                if v > u:
+                    current_node[dimension] += 1
+                else:
+                    current_node[dimension] -= 1
+            
+            # with wrapping around
+            else:
+
+                # determine whether increasing/decreasing component is required + break ties
+                if u >= v:
+
+                    # wrap around switch
+                    if current_node[dimension] == k - 1:
+                        current_node[dimension] = 0
+
+                    # normal incrementation
+                    else:
+                        current_node[dimension] += 1
+
+                else:
+
+                    # wrap around switch
+                    if current_node[dimension] == 0:
+                        current_node[dimension] = k - 1
+
+                    # normal decrementation
+                    else:
+                        current_node[dimension] -= 1
+            
+            # update route and dimension
+            route.append(current_node.copy())
+
+        dimension -= 1
+    
+    return route
+
+
+route = karyncube_routing(4, 6, [1, 4, 3, 2], [2, 1, 1, 3])
+for node in route:
+    print(node)
